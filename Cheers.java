f1d54b788b75b6256775ba002679bf318870642d
Larabruk <larabrukirer@gmail.com>
2024-11-20 16:51:30 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int num = Integer.parseInt(args[1]);
+            String UpperCaseWord = word.toUpperCase();
+            String changeToAn = "AEFHILMNORSX";
+            char c;
+
+            for (int i = 0; i < word.length(); i++){
+                c = UpperCaseWord.charAt(i);
+                if (changeToAn.indexOf(c) == -1){
+                System.out.println("Give me a " + UpperCaseWord.charAt(i) +": " + UpperCaseWord.charAt(i) + "!" );
+            } else {
+                System.out.println("Give me an " + UpperCaseWord.charAt(i) +": " + UpperCaseWord.charAt(i) + "!" );
+            }
+        }
+            System.out.println("What does it spell?");
+            for(int j = 0; j < num; j++){
+                System.out.println(UpperCaseWord + "!!!");
+            }
         }
 }
+
+
+
